fb39d14222a44a4bbcbf2d70c32237e4e53087aa
PraveenPra <prvp6501@gmail.com>
2023-09-06 00:23:01 +0530

    Getting mouse co-ordinates when mouse click and drawn over canvas

--- web/js/sketchPad.js
+++ web/js/sketchPad.js
@@ -1,6 +1,7 @@
 class SketchPad{
-    //container div where u want to have ur canvas created with size
+    
     constructor(container,size = 400){
+        //create a sketchpad canvas
         this.canvas = document.createElement('canvas');
         this.canvas.width = size;
         this.canvas.height = size;
@@ -10,5 +11,45 @@ class SketchPad{
         box-shadow:0 0 10px 2px black;`;
 
         container.appendChild(this.canvas);
+
+        this.ctx = this.canvas.getContext('2d');
+
+        //to store paths
+        this.paths = [];
+        this.isDrawing = false;
+
+        //#-private method; to draw
+        this.#addEventListeners();
+    }
+
+    #addEventListeners(){
+        this.canvas.onmousedown = (e)=>{
+           const mouse = getMousePosition(e);
+            this.paths = [mouse];
+        this.isDrawing = true;
+        }
+
+        this.canvas.onmousemove=(e)=>{
+            if(this.isDrawing){
+              
+                const mouse =  const mouse = getMousePosition(e);
+
+                this.paths.push(mouse);
+                console.log(this.paths.length)
+            }
+        }
+
+        this.canvas.onmouseup=()=>{
+            this.isDrawing = false;
+        }
+
+        #getMousePosition(e){
+            const rect = this.canvas.getBoundingClientRect();
+
+            return [
+                Math.round(e.clientX - rect.left),
+                Math.round(e.clientY - rect.top)
+            ];
+        }
     }
 }
