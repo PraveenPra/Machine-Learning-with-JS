e00dfc36aaffcf33a2c9bce75bbf515aa7b53acc
PraveenPra <prvp6501@gmail.com>
2023-09-07 00:09:43 +0530

    Added undo functionality

--- web/js/sketchPad.js
+++ web/js/sketchPad.js
@@ -1,6 +1,6 @@
-class SketchPad{
-    
-    constructor(container,size = 400){
+class SketchPad {
+
+    constructor(container, size = 400) {
         //create a sketchpad canvas
         this.canvas = document.createElement('canvas');
         this.canvas.width = size;
@@ -11,6 +11,12 @@ class SketchPad{
         box-shadow:0 0 10px 2px black;`;
 
         container.appendChild(this.canvas);
+        this.lineBreak = document.createElement('br');
+        container.appendChild(this.lineBreak);
+
+        this.undoBtn = document.createElement('button');
+        this.undoBtn.innerHTML = "UNDO";
+        container.appendChild(this.undoBtn);
 
         this.ctx = this.canvas.getContext('2d');
 
@@ -22,50 +28,55 @@ class SketchPad{
         this.#addEventListeners();
     }
 
-    #addEventListeners(){
-        this.canvas.onmousedown = (e)=>{
-           const mouse = this.#getMousePosition(e);
-           //store each mouse pos in the array - [[x1,y1],[x2,y2]...]
+    #addEventListeners() {
+        this.canvas.onmousedown = (e) => {
+            const mouse = this.#getMousePosition(e);
+            //store each mouse pos in the array - [[x1,y1],[x2,y2]...]
             this.paths.push([mouse]);
-        this.isDrawing = true;
+            this.isDrawing = true;
         }
 
-        this.canvas.onmousemove=(e)=>{
-            if(this.isDrawing){
-              
+        this.canvas.onmousemove = (e) => {
+            if (this.isDrawing) {
+
                 const mouse = this.#getMousePosition(e);
                 //for multiple paths, we want to push to the last path(i draw something and letgo. then again i draw from somewhere. both paths start at different places.)
-                const lastPath = this.paths[this.paths.length-1];
+                const lastPath = this.paths[this.paths.length - 1];
 
                 lastPath.push(mouse);
-               this.#reDraw();
+                this.#reDraw();
             }
         }
 
-        this.canvas.onmouseup=()=>{
+        this.canvas.onmouseup = () => {
             this.isDrawing = false;
         }
 
-        this.canvas.ontouchstart=(e)=>{
+        this.canvas.ontouchstart = (e) => {
             //on mobile screens,mouse events dont work, use the first touch and pass that event(loc) to the mousedown function to do same task of drawing
-            const loc=e.touches[0];
+            const loc = e.touches[0];
             this.canvas.onmousedown(loc);
         }
 
-        this.canvas.ontouchmove=(e)=>{
-            const loc=e.touches[0];
-            this.canvas.onmousemove(loc); 
+        this.canvas.ontouchmove = (e) => {
+            const loc = e.touches[0];
+            this.canvas.onmousemove(loc);
         }
 
-        this.canvas.ontouchend=(e)=> this.canvas.onmouseup();
+        this.canvas.ontouchend = (e) => this.canvas.onmouseup();
+
+        this.undoBtn.onclick=()=>{
+            this.paths.pop();
+            this.#reDraw();
+        }
     }
 
-    #reDraw(){
-        this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
-        draw.paths(this.ctx,this.paths);
+    #reDraw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        draw.paths(this.ctx, this.paths);
     }
 
-    #getMousePosition(e){
+    #getMousePosition(e) {
         const rect = this.canvas.getBoundingClientRect();
 
         return [
