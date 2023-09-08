8d0a59cdc7408f4426181a77df27381a60d8dade
PraveenPra <prvp6501@gmail.com>
2023-09-09 00:23:41 +0530

    Made a dataset_generator in node js
    run $node dataset_generator to create samples.json

---
+++ node/dataset_generator.js
@@ -0,0 +1,35 @@
+const constants = {};
+
+constants.DATA_DIR = '../data';
+constants.RAW_DIR = constants.DATA_DIR+'/raw';
+constants.DATASET_DIR = constants.DATA_DIR+'/dataset';
+constants.JSON_DIR = constants.DATASET_DIR+'/json';
+constants.IMG_DIR = constants.DATASET_DIR+'/img';
+constants.SAMPLES = constants.DATASET_DIR+'/samples.json';
+
+const fs = require('fs');
+
+//This gives an array of all the filenames.json in the raw folder
+const fileNames = fs.readdirSync(constants.RAW_DIR);
+const samples = [];
+let id = 1;
+
+fileNames.forEach(fn=>{
+    //this reads all the files in the raw folder giving the data object inside
+    const content = fs.readFileSync(
+        constants.RAW_DIR+'/'+fn
+    )
+
+    //for each drawings(fish,car..) make an object and write into a sample file
+    const {student,session,drawings} = JSON.parse(content);
+    for(let label in drawings){
+        samples.push({
+            id,label,
+            student_name : student,
+            student_id : session
+        })
+        id++;
+    }
+})
+
+fs.writeFileSync(constants.SAMPLES,JSON.stringify(samples));
